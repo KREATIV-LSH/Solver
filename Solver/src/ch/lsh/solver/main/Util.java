c9bb389fed1367c442b90e40548b64ce468e3f6a
KRTEATIV-LSH <lshutterli@gmail.com>
2021-10-20 14:55:53 +0200

    Added simple description to methodes in Util.java

--- Solver/src/ch/lsh/solver/main/Util.java
+++ Solver/src/ch/lsh/solver/main/Util.java
@@ -4,19 +4,23 @@ public class Util {
     
     public static StringBuilder mainSB;
 
+    // Sets up the stringbuilder
     public static void setup(int sbCapacity) {
         mainSB = new StringBuilder(sbCapacity);
     }
 
+    // Force gc cycle in jvm
     public static void garbageCollector() {
         System.gc();
         System.runFinalization();
     }
 
+    // Start printing by reseting StringBuilder
     public static void startPrint() {
         mainSB.setLength(0);
     }
 
+    // End printing by printing data that is in the StringBuiler
     public static void endPrint() {
         System.out.println(mainSB.toString());
     }
