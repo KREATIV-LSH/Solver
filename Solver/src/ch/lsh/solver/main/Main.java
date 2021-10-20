35ecd68a0e2c10a2bff38d0ee44450577af5735d
KRTEATIV-LSH <lshutterli@gmail.com>
2021-10-20 21:22:56 +0200

    Indentaion

--- Solver/src/ch/lsh/solver/main/Main.java
+++ Solver/src/ch/lsh/solver/main/Main.java
@@ -1,13 +1,13 @@
 package ch.lsh.solver.main;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Util.setup(16);
+	
+	public static void main(String[] args) {
+		Util.setup(16);
 
-        Util.startPrint();
-        Util.mainSB.append("Hello World!");
-        Util.endPrint();
-    }
+		Util.startPrint();
+		Util.mainSB.append("Hello World!");
+		Util.endPrint();
+	}
 
 }
--- Solver/src/ch/lsh/solver/main/Util.java
+++ Solver/src/ch/lsh/solver/main/Util.java
@@ -1,28 +1,28 @@
 package ch.lsh.solver.main;
 
 public class Util {
-    
-    public static StringBuilder mainSB;
+	
+	public static StringBuilder mainSB;
 
-    // Sets up the stringbuilder
-    public static void setup(int sbCapacity) {
-        mainSB = new StringBuilder(sbCapacity);
-    }
+	// Sets up the stringbuilder
+	public static void setup(int sbCapacity) {
+		mainSB = new StringBuilder(sbCapacity);
+	}
 
-    // Force gc cycle in jvm
-    public static void garbageCollector() {
-        System.gc();
-        System.runFinalization();
-    }
+	// Force gc cycle in jvm
+	public static void garbageCollector() {
+		System.gc();
+		System.runFinalization();
+	}
 
-    // Start printing by reseting StringBuilder
-    public static void startPrint() {
-        mainSB.setLength(0);
-    }
+	// Start printing by reseting StringBuilder
+	public static void startPrint() {
+		mainSB.setLength(0);
+	}
 
-    // End printing by printing data that is in the StringBuiler
-    public static void endPrint() {
-        System.out.println(mainSB.toString());
-    }
+	// End printing by printing data that is in the StringBuiler
+	public static void endPrint() {
+		System.out.println(mainSB.toString());
+	}
 
 }
