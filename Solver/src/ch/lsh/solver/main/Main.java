7af1673624b539ed03bf480cd1728672d26d8a10
KRTEATIV-LSH <lshutterli@gmail.com>
2021-10-20 14:23:05 +0200

    Init

---
+++ Solver/src/ch/lsh/solver/main/Main.java
@@ -0,0 +1,9 @@
+package ch.lsh.solver.main;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Solver/src/ch/lsh/solver/main/Util.java
@@ -0,0 +1,13 @@
+public class Util {
+    
+    public static StringBuilder mainSB;
+
+    public static void setup(int sbCapacity) {
+        mainSB = new StringBuilder(sbCapacity);
+    }
+
+    public static void printBuf() {
+
+    }
+
+}
